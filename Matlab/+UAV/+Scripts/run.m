f135893359225e9d99b83956e66de4bbb0bc38d3
Dan <doates625@gmail.com>
2020-01-07 18:05:09 -0500

    Replace uav run scripts with package scripts

---
+++ Matlab/+UAV/+Scripts/run.m
@@ -0,0 +1,35 @@
+function run(uav, cmd_src)
+%RUN(uav, cmd_src, acc_max, wz_max) Run UAV interface
+%   Inputs:
+%       uav = UAV interface [UAV.Interfaces.Interface]
+%       cmd_src = Command source [UAV.CmdSrcs.CmdSrc]
+%   Author: Dan Oates (WPI Class of 2020)
+
+% Default args
+if nargin < 2, cmd_src = UAV.CmdSrcs.Xbox(); end
+if nargin < 1, uav = UAV.Interfaces.Sims.Matlab(); end
+
+% Create GUI
+fig = figure(1);
+gui = UAV.GUI(fig);
+
+% Loop timer
+timer = Timer();
+
+% Command loop
+while true
+    
+    % Loop routines
+    t = timer.toc();
+    cmd = cmd_src.get_cmd(t);
+    state = uav.update(cmd);
+    gui.update(state, cmd, t);
+    drawnow
+    
+    % Exit condition
+    if cmd_src.get_stop()
+        break
+    end
+end
+
+end
