7f6b93a51256485bbd76b2ce3fdc368e37256eec
Dan <doates625@gmail.com>
2020-01-07 18:08:52 -0500

    Minor refactors and lowered default poles

--- Matlab/+UAV/default_arg.m
+++ Matlab/+UAV/default_arg.m
@@ -2,7 +2,7 @@ function arg = default_arg(name)
 %DEFAULT_ARG Get default function arg by name
 %   
 %   arg = DEFAULT_ARG('model') UAV model [UAV.Models.Model]
-%   arg = DEFAULT_ARG('model_phys') UAV physical model [UAV.Models.Phys]
+%   arg = DEFAULT_ARG('phys_model') UAV physical model [UAV.Models.Phys]
 %   arg = DEFAULT_ARG('I_xx') Inertia x-axis [kg*m^2]
 %   arg = DEFAULT_ARG('I_yy') Inertia y-axis [kg*m^2]
 %   arg = DEFAULT_ARG('I_zz') Inertia z-axis [kg*m^2]
@@ -12,7 +12,7 @@ function arg = default_arg(name)
 %   arg = DEFAULT_ARG('mass') Total mass [kg]
 %   arg = DEFAULT_ARG('f_min') Min prop force [N]
 %   arg = DEFAULT_ARG('f_max') Max prop force [N]
-%   arg = DEFAULT_ARG('model_ctrl') UAV control model [UAV.Models.Ctrl]
+%   arg = DEFAULT_ARG('ctrl_model') UAV control model [UAV.Models.Ctrl]
 %   arg = DEFAULT_ARG('s_qx') Quaternion x-axis pole [s^-1]
 %   arg = DEFAULT_ARG('s_qy') Quaternion y-axis pole [s^-1]
 %   arg = DEFAULT_ARG('s_qz') Quaternion z-axis pole [s^-1]
@@ -23,14 +23,14 @@ function arg = default_arg(name)
 %   arg = DEFAULT_ARG('bt_name') Bluetooth device name [char]
 %   arg = DEFAULT_ARG('sim_port') Simulation serial port name [char]
 %   arg = DEFAULT_ARG('acc_max') Max acceleration cmd [m/s^2]
-%   arg = DEFAULT_ARG('wz_max') Max heading cmd velocity [rad/s]
+%   arg = DEFAULT_ARG('wz_max') Max heading cmd rate [rad/s]
 
 switch name
     % Full model
     case 'model', arg = UAV.Models.Model();
     
     % Physical model
-    case 'model_phys', arg = UAV.Models.Phys();
+    case 'phys_model', arg = UAV.Models.Phys();
     case 'I_xx', arg = 1.15e-03;
     case 'I_yy', arg = 1.32e-03;
     case 'I_zz', arg = 2.24e-03;
@@ -42,11 +42,11 @@ switch name
     case 'f_max', arg = 2.46;
         
     % Control model
-    case 'model_ctrl', arg = UAV.Models.Ctrl();
-    case 's_qx', arg = -5.0;
-    case 's_qy', arg = -5.0;
-    case 's_qz', arg = -5.0;
-    case 's_az', arg = -5.0;
+    case 'ctrl_model', arg = UAV.Models.Ctrl();
+    case 's_qx', arg = -3.0;
+    case 's_qy', arg = -3.0;
+    case 's_qz', arg = -3.0;
+    case 's_az', arg = -3.0;
     case 'fr_min', arg = 0.1;
     case 'fr_max', arg = 0.9;
     
@@ -62,7 +62,7 @@ switch name
     case 'wz_max', arg = pi/4;
         
     % Invalid name
-    otherwise, error('Invalid name.');
+    otherwise, error('Invalid name: %s', name);
 end
 
 end
