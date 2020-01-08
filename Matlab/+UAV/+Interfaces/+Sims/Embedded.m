c7856eb121121e0a3dad17ccfc1897bd6779cda8
Dan <doates625@gmail.com>
2020-01-07 19:27:41 -0500

    Added remote control and embedded simulator interfaces

---
+++ Matlab/+UAV/+Interfaces/+Sims/Embedded.m
@@ -0,0 +1,111 @@
+classdef Embedded < UAV.Interfaces.Sims.Sim
+    %EMBEDDED Embedded simulator for UAV model
+    %   Author: Dan Oates (WPI Class of 2020)
+    
+    properties (Access = protected, Constant)
+        start_byte = hex2dec('FF');     % Msg start byte 
+        msg_id_update = hex2dec('00');  % Data msg ID
+        baud_rate = 115200;             % Serial baud rate
+    end
+    
+    properties (Access = protected)
+        remote;     % UAV remote controller [UAV.Interfaces.Remote]
+        server;     % Serial interface [SerialServer]
+        got_resp;   % Response flag [logical]
+        f_prop;     % Prop forces [N]
+    end
+    
+    methods (Access = public)
+        function obj = Embedded(phys_model, f_sim, remote, sim_port)
+            %obj = EMBEDDED(phys_model, f_sim, remote, sim_port)
+            %   Construct UAV embedded simulator
+            %   Inputs:
+            %       phys_model = UAV physical model [UAV.Models.Phys]
+            %       f_sim = Simulation frequency [Hz]
+            %       remote = UAV remote controller [UAV.Interfaces.Remote]
+            %       sim_port = USB serial port name [char]
+            
+            % Default args
+            import('UAV.default_arg');
+            if nargin < 4, sim_port = default_arg('sim_port'); end
+            if nargin < 3, remote = default_arg('remote'); end
+            if nargin < 2, f_sim = default_arg('f_sim'); end
+            if nargin < 1, phys_model = default_arg('phys_model'); end
+            
+            % Copy properties
+            obj = obj@UAV.Interfaces.Sims.Sim(phys_model, f_sim);
+            obj.remote = remote;
+            
+            % Set up simulation serial server
+            serial_ = make_serial(sim_port, obj.baud_rate);
+            obj.server = SerialServer(serial_, obj.start_byte);
+            obj.server.add_tx(obj.msg_id_update, 40, @obj.msg_tx_update);
+            obj.server.add_rx(obj.msg_id_update, 16, @obj.msg_rx_update);
+        end
+        
+        function state = update(obj, cmd)
+            %state = UPDATE(obj, cmd)
+            %   Send commands and get new state
+            %   Inputs:
+            %       cmd = UAV command [UAV.Cmd]
+            %   Outputs:
+            %       state = UAV state [UAV.State]
+            
+            % Transmit commands and state data
+            obj.remote.update(cmd);
+            obj.server.tx(obj.msg_id_update);
+            
+            % Get force commands
+            obj.got_resp = false;
+            while ~obj.got_resp
+                obj.server.rx();
+            end
+            
+            % Simulate dynamics
+            state = obj.update_sim(obj.f_prop, cmd.state);
+        end
+        
+        function delete(obj)
+            %DELETE(obj) Disconnects from serial port
+            fclose(obj.server.get_serial());
+        end
+    end
+    
+    methods (Access = protected)
+        function msg_tx_update(obj, server)
+            %MSG_TX_DATA(obj, server) Packs IMU reading TX message
+            %   Data format:
+            %   [00-03] Quat-w [float]
+            %   [04-07] Quat-x [float]
+            %   [08-11] Quat-y [float]
+            %   [12-15] Quat-z [float]
+            %   [16-19] Omega-x [float, rad/s]
+            %   [20-23] Omega-y [float, rad/s]
+            %   [24-27] Omega-z [float, rad/s]
+            %   [28-31] Local accel-x [float, m/s^2]
+            %   [32-35] Local accel-y [float, m/s^2]
+            %   [36-39] Local accel-z [float, m/s^2]
+            str = Struct();
+            str.set(obj.state.ang_pos.vector(), 'single');
+            str.set(obj.state.ang_vel, 'single');
+            acc_loc = obj.state.ang_pos.inv().rotate(obj.state.lin_acc);
+            str.set(acc_loc, 'single');
+            server.set_tx_data(str.get_buffer());
+        end
+        
+        function msg_rx_update(obj, server)
+            %MSG_RX_DATA(obj, server) Unpacks force RX message
+            %   Data format:
+            %   [00-03] Force++ [float, N]
+            %   [04-07] Force+- [float, N]
+            %   [08-11] Force-+ [float, N]
+            %   [12-15] Force-- [float, N]
+            str = Struct(server.get_rx_data());
+            for i = 1:4
+                obj.f_prop(i) = str.get('single');
+            end
+            obj.got_resp = true;
+        end
+    end
+end
+
---
+++ Matlab/+UAV/+Interfaces/Remote.m
@@ -0,0 +1,141 @@
+classdef Remote < UAV.Interfaces.Interface
+    %REMOTE Bluetooth remote control for UAV
+    %   Author: Dan Oates (WPI Class of 2020)
+    
+    properties (Access = protected, Constant)
+        start_byte = hex2dec('FF');     % Msg start byte
+        msg_id_state = hex2dec('00');   % State cmd ID
+        msg_id_update = hex2dec('01');  % Update msg ID
+    end
+    
+    properties (Access = protected)
+        server;     % Serial interface [SerialServer]
+        got_resp;   % Response flag [logical]
+        cmd;        % UAV command [UAV.Cmd]
+    end
+    
+    methods (Access = public)
+        function obj = Remote(bt_name)
+            %obj = REMOTE(bt_name) Construct UAV remote with given bluetooth name [char]
+            
+            % Default args
+            if nargin < 1
+                bt_name = UAV.default_arg('bt_name');
+            end
+            
+            % Superconstructor
+            obj = obj@UAV.Interfaces.Interface();
+
+            % Set up serial server
+            bluetooth_ = make_bluetooth(bt_name);
+            obj.server = SerialServer(bluetooth_, obj.start_byte);
+            obj.server.add_tx(obj.msg_id_state, 0, @obj.msg_tx_state);
+            obj.server.add_tx(obj.msg_id_update, 16, @obj.msg_tx_update);
+            obj.server.add_rx(obj.msg_id_update, 57, @obj.msg_rx_update);
+        end
+        
+        function state = update(obj, cmd)
+            %state = UPDATE(obj, cmd)
+            %   Send commands and get new state
+            %   Inputs:
+            %       cmd = UAV command [UAV.Cmd]
+            %   Outputs:
+            %       state = UAV state [UAV.State]
+            
+            % Transmit commands
+            obj.cmd = cmd;
+            if ~strcmp(obj.state.state, obj.cmd.state)
+                obj.server.tx(obj.msg_id_state);
+            end
+            obj.server.tx(obj.msg_id_update);
+            
+            % Wait for response
+            obj.got_resp = false;
+            while ~obj.got_resp
+                obj.server.rx();
+            end
+            
+            % Return state
+            state = obj.state;
+        end
+        
+        function delete(obj)
+            %DELETE(obj) Disonnects from Bluetooth
+            fclose(obj.server.get_serial());
+        end
+    end
+    
+    methods (Access = protected)
+        function msg_tx_state(obj, server)
+            %MSG_TX_STATE(obj, server) Packs State TX message
+            switch obj.cmd.state
+                case 'Enabled', state_cmd = 0;
+                case 'Disabled', state_cmd = 1;
+                case 'Failed', state_cmd = 2;
+            end
+            server.set_tx_data(state_cmd);
+        end
+        
+        function msg_tx_update(obj, server)
+            %MSG_TX_UPDATE(obj, server) Packs Update TX message
+            %   Data format:
+            %   [00-03] Global accel x [single, m/s^2]
+            %   [04-07] Global accel y [single, m/s^2]
+            %   [08-11] Global accel z [single, m/s^2]
+            %   [12-15] Heading cmd [single, rad]
+            str = Struct();
+            str.set(obj.acc_cmd, 'single');
+            str.set(obj.tz_cmd, 'single');
+            server.set_tx_data(str.get_buffer());
+        end
+        
+        function msg_rx_update(obj, server)
+            %MSG_RX_UPDAT(obj, server) Unpacks Update RX message
+            %   Data format:
+            %   [00-03] Quat-w [single]
+            %   [04-07] Quat-x [single]
+            %   [08-11] Quat-y [single]
+            %   [12-15] Quat-z [single]
+            %   [16-19] Omega-x [single, rad/s]
+            %   [20-23] Omega-y [single, rad/s]
+            %   [24-27] Omega-z [single, rad/s]
+            %   [28-31] Accel-x [single, m/s^2]
+            %   [32-35] Accel-y [single, m/s^2]
+            %   [36-39] Accel-z [single, m/s^2]
+            %   [40-43] Force++ [single, N]
+            %   [44-47] Force+- [single, N]
+            %   [48-51] Force-+ [single, N]
+            %   [52-55] Force-- [single, N]
+            %   [56-56] State [uint8, enum]
+            %       0x00 = Enabled
+            %       0x01 = Disabled
+            %       0x02 = Failed
+            
+            % Struct unpacker
+            str = Struct(server.get_rx_data());
+            
+            % Unpack numeric data
+            arr = zeros(14, 1);
+            for i = 1:length(arr)
+                arr(i) = str.get('single');
+            end
+            ang_pos = Quat(arr(1:4));
+            ang_vel = arr(5:7);
+            lin_acc = ang_pos.rotate(arr(8:10));
+            f_prop = arr(11:14);
+            
+            % Unpack state
+            state_byte = str.get('uint8');
+            switch (state_byte)
+                case 0, state = 'Enabled';
+                case 1, state = 'Disabled';
+                case 2, state = 'Failed';
+            end
+            
+            % Set state and response flag
+            obj.state = UAV.State(ang_pos, ang_vel, lin_acc, f_prop, state);
+            obj.got_resp = true;
+        end
+    end
+end
+
