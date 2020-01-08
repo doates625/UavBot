faea388c8d6b36ae469956b41ae89e51c54fedec
Dan <doates625@gmail.com>
2020-01-07 23:23:43 -0500

    Added instrreset to help with bluetooth errors

--- Matlab/+UAV/+Scripts/run_em_ts.m
+++ Matlab/+UAV/+Scripts/run_em_ts.m
@@ -2,6 +2,7 @@ function run_em_ts()
 %RUN_EM_TS() Run UAV embedded simulator with time sequence control
 
 clc
+instrreset
 uav = UAV.Interfaces.Sims.Embedded();
 cmd_src = UAV.CmdSrcs.TimeSeq();
 UAV.Scripts.run(uav, cmd_src);
